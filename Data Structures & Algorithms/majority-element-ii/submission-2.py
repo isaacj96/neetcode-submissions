f9a481c5d85f75962eb253c0cf1490eccc1e09d1
Isaac Jaramillo <isaac.jaramillo1996@gmail.com>
2026-04-17 14:38:44 -0500

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        modedict = defaultdict(int)
+        more = len(nums) / 3
+        modeset = set()
+
+        for i in nums:
+            modedict[i] += 1
+            if modedict[i] > more:
+                modeset.add(i)
+
+        return list(modeset)
